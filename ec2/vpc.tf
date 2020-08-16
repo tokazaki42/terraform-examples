7d44ef8346da4f5240ee6766d3f9b39519256fde
tomy <okazaki.nsw@gmail.com>
2020-08-16 12:22:27 +0900

    update readmes

--- ec2/vpc.tf
+++ ec2/vpc.tf
@@ -26,7 +26,7 @@ resource "aws_route_table" "public" {
 resource "aws_subnet" "public_a" {
   vpc_id            = aws_vpc.example-vpc.id
   cidr_block        = "10.10.1.0/24"
-  availability_zone = "ap-northeast-1a"
+  availability_zone = "${var.region}a"
 
   tags = {
     Name = "public-a"
